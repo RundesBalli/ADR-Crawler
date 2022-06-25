cf73b019323c9cb4a1b5a80223fe8d59ccc508ac
RundesBalli <GitHub@RundesBalli.com>
2022-06-26 00:16:49 +0200

    Forgot auto_increment

--- at/adr.sql
+++ at/adr.sql
@@ -7,16 +7,17 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP DATABASE IF EXISTS `strahlen_at`;
-CREATE DATABASE `strahlen_at` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `strahlen_at`;
+DROP DATABASE IF EXISTS `adr`;
+CREATE DATABASE `adr` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `adr`;
 
 DROP TABLE IF EXISTS `entries`;
 CREATE TABLE `entries` (
-  `id` int(10) unsigned NOT NULL COMMENT 'ID',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `stationId` int(10) unsigned NOT NULL COMMENT 'stations.id',
   `timestamp` datetime NOT NULL COMMENT 'Time of the measurement',
   `value` double(9,1) unsigned NOT NULL COMMENT 'The measured value',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `stationId_timestamp` (`stationId`,`timestamp`),
   KEY `timestamp` (`timestamp`),
   KEY `value` (`value`),
@@ -34,4 +35,4 @@ CREATE TABLE `stations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Measuring stations';
 
 
--- 2022-06-25 21:26:44
+-- 2022-06-25 22:15:22
